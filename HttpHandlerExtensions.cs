33f2e87c2dddd657cdf9cb3061fdbd4f62520d47
Robert Roose <robert@shivercube.com>
2020-05-17 19:36:02 -0300

    Copied over existing code from agrix

---
+++ HttpHandlerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MockHttpServer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MockHTTP.NET
+{
+    /// <summary>
+    /// Provides an extension method to convert CustomMockHttpHandler instances
+    /// to MockHttpHandler for use by MockServer.
+    /// </summary>
+    internal static class HttpHandlerExtensions
+    {
+        /// <summary>
+        /// Converts to a collection of MockHttpHandler instances.
+        /// </summary>
+        /// <param name="handlers">The handlers to convert.</param>
+        /// <returns>The converted instances.</returns>
+        public static IEnumerable<MockHttpHandler> GetMockHttpHandlers(
+            this IEnumerable<HttpHandler> handlers)
+        {
+            return handlers.Select(handler => handler.MockHttpHandler).ToList();
+        }
+    }
+}
---
+++ HttpMethods.cs
@@ -0,0 +1,12 @@
+﻿namespace MockHTTP.NET
+{
+    /// <summary>
+    /// The types of HTTP methods available to mock
+    /// </summary>
+    public enum HttpMethods
+    {
+        Get,
+        Post,
+        Put
+    }
+}
