e38e47361224df5a3f1a605eeacc7c53d63e4076
Robert Roose <robert@shivercube.com>
2020-05-19 13:49:55 -0300

    Creating basic server

---
+++ Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System;
+
+namespace Server
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            using var server = new Server();
+
+            try
+            {
+                while (true)
+                    Thread.Sleep(10000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace Server
+{
+    internal class Server : IDisposable
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Task _task;
+
+        public Server()
+        {
+            _listener.Prefixes.Add("http://localhost:8000/");
+            _listener.Start();
+            _task = HandleRequests();
+        }
+
+        private async Task HandleRequests()
+        {
+            while (_listener.IsListening)
+            {
+                var context = await _listener.GetContextAsync();
+                var buffer = Encoding.UTF8.GetBytes("hello");
+                context.Response.ContentLength64 += buffer.Length;
+                await context.Response.OutputStream.WriteAsync(
+                    buffer, 0, buffer.Length);
+                context.Response.OutputStream.Close();
+                context.Response.Close();
+            }
+        }
+
+        public void Dispose()
+        {
+            _listener.Stop();
+            _task.Dispose();
+        }
+    }
+}
